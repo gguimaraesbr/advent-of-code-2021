ee6a2844a15e224a4ee4426f0378294f1dc4bb12
Guilherme Filho <gguimaraesbr@gmail.com>
2021-12-02 13:06:10 -0300

    add tests and a little bit more

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,13 +1,13 @@
-use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    println!("Day 01 First Star: {} measurements are larger than the previous measurement", first_star("src/inputs/day01/puzzle.txt"));
+    println!("Day 01 Second Star: {} sums are larger than the previous sum", second_star("src/inputs/day01/puzzle.txt"));
+}
 
-    let filename = &args[1];
-
+fn first_star(filename: &str) -> u32 {
     let contents = File::open(filename)
         .expect("file not found.");
 
@@ -18,25 +18,50 @@ fn main() {
         .map(|line| line.unwrap().parse::<u32>().unwrap())
         .collect();
 
-    part_one(&depths);
-
-    part_two(&depths);
+    return depths.windows(2).filter(|w| w[0] < w[1]).count().try_into().unwrap();
 }
 
-fn part_one(depths: &Vec<u32>) {
-    let increases = depths.windows(2).filter(|w| w[0] < w[1]).count();
+fn second_star(filename: &str) -> u32 {
+    let contents = File::open(filename)
+        .expect("file not found.");
 
-    println!("{} measurements are larger than the previous measurement", increases);
-}
+    let reader = BufReader::new(contents);
 
-fn part_two(depths: &Vec<u32>) {
+    let depths: Vec<u32> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<u32>().unwrap())
+        .collect();
+
     let mut sums = Vec::new();
 
     for window in depths.windows(3) {
         sums.push(window.iter().sum::<u32>());
     }
 
-    let increases = sums.windows(2).filter(|w| w[0] < w[1]).count();
+    return sums.windows(2).filter(|w| w[0] < w[1]).count().try_into().unwrap();
+}
 
-    println!("{} sums are larger than the previous sum", increases);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn first_star_with_sample_data() {
+        assert_eq!(first_star("src/inputs/day01/sample.txt"), 7);
+    }
+
+    #[test]
+    fn first_star_with_puzzle_data() {
+        assert_eq!(first_star("src/inputs/day01/puzzle.txt"), 1167);
+    }
+
+    #[test]
+    fn second_star_with_sample_data() {
+        assert_eq!(second_star("src/inputs/day01/sample.txt"), 5);
+    }
+
+    #[test]
+    fn second_star_with_puzzle_data() {
+        assert_eq!(second_star("src/inputs/day01/puzzle.txt"), 1130);
+    }
 }
